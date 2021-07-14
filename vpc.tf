9ba152c5074bc8fa74451b7b45245b45bea43171
Wes Bustraan <wes.bustraan@gfs.com>
2021-07-14 19:41:23 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,13 @@
+module "network" {
+  source       = "app.terraform.io/wjb-training/network/google"
+  version      = "3.2.2"
+  network_name = "gaurav-network"
+  project_id   = var.project
+  subnets = [
+    {
+      subnet_name   = "gaurav-subnet"
+      subnet_ip     = "10.100.10.0/24"
+      subnet_region = var.region
+    }
+  ]
+}
